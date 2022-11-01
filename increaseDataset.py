54c88b18869a1375b3509bbd3b4848fd8b3ec428
MarkCoulon <88473455+MarkCoulon@users.noreply.github.com>
2022-11-01 10:14:53 +1000

    Update increaseDataset.py

--- increaseDataset.py
+++ increaseDataset.py
@@ -1,14 +1,16 @@
 import numpy as np
 import json
+import sys
 
-'''
-this file is used to increase the dataset of a file based on a choicePercentage, e.g. 0.1 means 10%. The ogFile is the
-original file and the randomDataset is the file that the new passwords will come from
-'''
+ogFile = sys.argv[1]
+# ogFile = "data/cleanmyspace.txt"
+randomDataset = sys.argv[2]
+# randomDataset = "data/cleanRockYou.txt"
+choicePercent = int(sys.argv[3])
+# choicePercent = 0.1
 
-ogFile = "data/cleanmyspace.txt"
-randomDataset = "data/cleanRockYou.txt"
-choicePercent = 0.1
+print("Choice percent is " + str(choicePercent) + "%")
+print(int(choicePercent))
 
 with open(ogFile, encoding='latin1') as f:
     newP = f.readlines()
@@ -20,33 +22,42 @@ with open(randomDataset, encoding='latin1') as f:
     newR = f.readlines()
 randomWords = np.array(newR)
 
-randomChoice = int(len(ogWords) * choicePercent)
-# randomChoice = 5
+randomChoice = int(len(ogWords) * (choicePercent/100))
 
-print(ogWords)
+
 print("Number of words to add is: " + str(randomChoice))
-AddingWords = np.random.choice(a=randomWords, size=randomChoice, replace=False)
+if randomChoice < len(randomWords):
+  AddingWords = np.random.choice(a=randomWords, size=randomChoice, replace=False)
+  # print(AddingWords)
 
-print(AddingWords)
+  for word in AddingWords:
+      if word in ogWords:  # we have to get a new one
+          alreadyInFlag = True
+          
+          while alreadyInFlag:
+              print(str(word) + " Word already in ogWords")
+              newRandom = np.random.choice(a=randomWords, size=1, replace=False)
+              print(newRandom)
+              if newRandom not in ogWords:
+                  ogWords = np.append(ogWords, newRandom)
+                  alreadyInFlag = False
+      else:
+          ogWords = np.append(ogWords, word)
+else:
+  print("Other file is too small for random choice, adding all non duplicates")
+  main_list = list(set(randomWords) - set(ogWords))
+  print("Adding " + str(len(main_list)))
+  print(main_list)
+  ogWords = np.append(ogWords, main_list)
+  print(ogWords)
 
-for word in AddingWords:
-    if word in ogWords:  # we have to get a new one
-        alreadyInFlag = True
-        print(word)
-        while alreadyInFlag:
-            print("Word already in ogWords")
-            newRandom = np.random.choice(a=randomWords, size=1, replace=False)
-            print(newRandom)
-            if newRandom not in ogWords:
-                ogWords = np.append(ogWords, newRandom)
-                alreadyInFlag = False
-    else:
-        ogWords = np.append(ogWords, word)
 
-newFileName = ogFile.split("/")[1].split(".txt")[0] + "Extended.txt"
+
+newFileName = "data/" + ogFile.split("/")[1].split(".txt")[0] + "Extended_" + str(choicePercent) + "_" + randomDataset.split("/")[1].split(".txt")[0] + ".txt"
 
 print("New length of words is " + str(len(ogWords)))
 finalWords = list(ogWords)
+print("New filename is " + str(newFileName))
 with open(newFileName, 'w', encoding='latin1') as f:
     # f.write(json.dumps(finalWords))
     f.writelines(finalWords)
